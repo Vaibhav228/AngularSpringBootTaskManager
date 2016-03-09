b9314cdb9fa922b29b3032179108f6121484a5f1
3658515 <vaibhav.patil@westernunion.com>
2016-03-09 17:53:43 +0530

    Added login page and spinner

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,103 +1,54 @@
-var taskManagerModule = angular.module('taskManagerApp', ['ngAnimate']);
 
-taskManagerModule.controller('taskManagerController', function ($scope,$http) {
-	
-	var urlBase="";
-	$scope.toggle=true;
-	$scope.selection = [];
-	$scope.statuses=['ACTIVE','COMPLETED'];
-	$scope.priorities=['HIGH','LOW','MEDIUM'];
-	$http.defaults.headers.post["Content-Type"] = "application/json";
+angular
 
-    function findAllTasks() {
-        //Get all tasks and display initially
-        $http.get(urlBase + '/tasks/search/findByTaskArchived?archivedfalse=0').
-            success(function (data) {
-                if (data._embedded != undefined) {
-                    $scope.tasks = data._embedded.tasks;
-                } else {
-                    $scope.tasks = [];
-                }
-                for (var i = 0; i < $scope.tasks.length; i++) {
-                    if ($scope.tasks[i].taskStatus == 'COMPLETED') {
-                        $scope.selection.push($scope.tasks[i].taskId);
-                    }
-                }
-                $scope.taskName="";
-                $scope.taskDesc="";
-                $scope.taskPriority="";
-                $scope.taskStatus="";
-                $scope.toggle='!toggle';
-            });
-    }
+.module('taskManagerApp', ['ngAnimate','ngRoute'])
 
-    findAllTasks();
+.config(function ($routeProvider, $httpProvider) {
+    $routeProvider
+    .when('/', {
+        redirectTo: '/login'
+    })
+    .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginController'
+    })
+    .when('/task', {
+        templateUrl: 'views/task.html',
+        controller: 'TaskManagerController'
+    })
+    .otherwise({
+        redirectTo: '/'
+    })
+})
 
-	//add a new task
-	$scope.addTask = function addTask() {
-		if($scope.taskName=="" || $scope.taskDesc=="" || $scope.taskPriority == "" || $scope.taskStatus == ""){
-			alert("Insufficient Data! Please provide values for task name, description, priortiy and status");
+.run(function ($rootScope, $location) {
+	//$rootScope.sessionId = ContextService.getSessionId();
+	$rootScope.sessionId = "1";
+
+	$rootScope.$on('$locationChangeStart', function (event, next, current) {
+		//$rootScope.loggedInUser = ContextService.getLoggedInUser();
+		$rootScope.loggedInUser = "Vaibhav";
+		console.log('URL change requested...')
+		console.log($location.path())
+		console.log($rootScope.context)
+
+		if ($location.path() !== '/login' && !$rootScope.loggedInUser && $location.path() !== '/forgetpassword') {
+			console.log('Route to login');
+			$location.path('/login');
+			return;
 		}
-		else{
-		 $http.post(urlBase + '/tasks', {
-             taskName: $scope.taskName,
-             taskDescription: $scope.taskDesc,
-             taskPriority: $scope.taskPriority,
-             taskStatus: $scope.taskStatus
-         }).
-		  success(function(data, status, headers) {
-             var newTaskUri = headers()["location"];
-             console.log("Might be good to GET " + newTaskUri + " and append the task.");
-             // Refetching EVERYTHING every time can get expensive over time
-             // Better solution would be to $http.get(headers()["location"]) and add it to the list
-             findAllTasks();
-		    });
-		}
-	};
-		
-	// toggle selection for a given task by task id
-	  $scope.toggleSelection = function toggleSelection(taskUri) {
-	    var idx = $scope.selection.indexOf(taskUri);
 
-	    //Is currently selected HTTP PATCH to ACTIVE state
-	    if (idx > -1) {
-	      $http.patch(taskUri, { taskStatus: 'ACTIVE' }).
-		  success(function(data) {
-			  console.log("Task unmarked");
-              findAllTasks();
-		    });
-	      $scope.selection.splice(idx, 1);
-	    }
+		/* if (ContextService.isOnline() == false && $location.path() == '/queue/list') {
+              console.log('Offline login...Routing to schedule next page...');
+              $location.path('/trip/list');
+              return;
+          }*/
 
-	    // is newly selected
-        // HTTP PATCH to COMPLETED state
-	    else {
-	      $http.patch(taskUri, { taskStatus: 'COMPLETED' }).
-		  success(function(data) {
-			  console.log("Task marked completed");
-              findAllTasks();
-		    });
-	      $scope.selection.push(taskUri);
-	    }
-	  };
-	  
-	
-	// Archive Completed Tasks
-	  $scope.archiveTasks = function archiveTasks() {
-          $scope.selection.forEach(function(taskUri) {
-              if (taskUri != undefined) {
-                  $http.patch(taskUri, { taskArchived: 1});
-              }
-          });
-          console.log("Successfully Archived");
-          console.log("It's risky to run this without confirming all the patches are done. when.js is great for that");
-          findAllTasks();
-	  };
-	
-});
+		console.log('no go to loop');
+	});
+})
 
-//Angularjs Directive for confirm dialog box
-taskManagerModule.directive('ngConfirmClick', [
+.directive('ngConfirmClick', [
 	function(){
          return {
              link: function (scope, element, attr) {
